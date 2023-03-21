46b9675e3e45de774499e17c52eb097e70660362
pranalinarkhede <pranalipnarkhede@gmail.com>
2023-03-21 14:38:03 +0530

    Update rectangle_property.py

--- rectangle_property.py
+++ rectangle_property.py
@@ -1,2 +1,30 @@
 # property concept in python
 
+class Rectangle:
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+    
+    @property
+    def width(self):
+        print("Width : ",self._width)       
+    @width.setter
+    def width(self, value):
+        if value <= 0:
+            raise ValueError("Width must be positive")
+        self._width = value
+    
+    @property
+    def height(self):
+        print("Height : ",self._height)   
+    @height.setter
+    def height(self, value):
+        if value <= 0:
+            raise ValueError("Height must be positive")
+        self._height = value
+    
+    
+
+r = Rectangle(32, 48)
+r.width
+r.height
