a830cf1634f850abbe34a06997dc637dee43ad9c
pranalinarkhede <pranalipnarkhede@gmail.com>
2023-03-21 10:45:53 +0530

    added cls decorator in types_decorator.py

--- types_decorators.py
+++ types_decorators.py
@@ -14,3 +14,20 @@ def f2():
     print("Hello, in function f2")
     
 f2()
+
+# 2. class decorator
+print("\nClass Decorator :")
+def cls_decorator(cls):
+    cls.new_attr = "new attribute"
+    return cls
+
+@cls_decorator
+class MyClass:
+    attr = "old attribute"
+    def my_method(self):
+        print("Hello from MyClass")
+
+obj = MyClass()
+print(obj.attr)
+print(obj.new_attr)
+obj.my_method()
