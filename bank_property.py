f16efff0a21067b1f9ea06b078d4114a452f85e0
pranalinarkhede <pranalipnarkhede@gmail.com>
2023-03-21 18:03:47 +0530

    Update bank_property.py

--- bank_property.py
+++ bank_property.py
@@ -19,3 +19,16 @@ class BankAccount:
     def account_type(self):
         return self._account_type
     
+    def deposit(self, amount):
+        if amount < 0:
+            raise ValueError("Deposit amount must be positive")
+        self._balance += amount
+    
+    def withdraw(self, amount):
+        if amount < 0:
+            raise ValueError("Withdrawal amount must be positive")
+        if amount > self._balance:
+            raise ValueError("Insufficient funds")
+        self._balance -= amount
+        
+    
